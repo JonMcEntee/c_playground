9a26af512e8d3d3d99444ad352e4e6e19e0860c7
Jonathan McEntee <me@linuxbox.io>
2017-02-16 18:04:50 +0900

    add hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,9 @@
+// a "Hello world" program used to demonstrate that C++ code can be properly
+// compiled and executed on your computer.
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}
